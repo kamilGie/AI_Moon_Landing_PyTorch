99b3bad709d7098a2d7b762a601bf28757e1c8ca
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-08-07 13:08:38 +0200

    finnal commit

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ class ReplayMemory(object):
         dones = torch.from_numpy(np.vstack([e[4] for e in experiences if e is not None]).astype(np.uint8)).float().to(self.device)
         return states,next_states,actions,rewards,dones
     
-class agent():
+class Agent():
     def __init__(self,states_size,action_size):
         self.device  = torch.device("cuda:0"if torch.cuda.is_available() else "cpu")
         self.states_size = states_size
@@ -99,7 +99,7 @@ class agent():
         states,next_state,action,rewards,dones = experienes
         next_q_target = self.target_qnetork(next_state).detach().max(1)[0].unsqueeze(1)
         q_target = rewards + (discount_factor * next_q_target * (1- dones))
-        q_expected = self.local_qnetork(states).gather(1,actions)
+        q_expected = self.local_qnetork(states).gather(1,action)
         loss = F.mse_loss(q_expected, q_target)
         self.optimizer.zero_grad()
         loss.backward()
@@ -108,4 +108,90 @@ class agent():
 
     def soft_update(self ,localModel, TargetModel, interpolation_parameter):
         for targetParam, local_param in zip(TargetModel.parameters(),localModel.parameters()):
-            targetParam.data.copy_(interpolation_parameter* local_param.data + (1.0-interpolation_parameter) * targetParam.data) 
+            targetParam.data.copy_(interpolation_parameter* local_param.data + (1.0-interpolation_parameter) * targetParam.data)
+
+agent = Agent(state_size, number_actions)
+
+
+number_episozed = 2000
+maximum_number_timesteps_per_episodes = 1000
+epssilon_starting_value = 1.0 
+epssilon_ending_value = 0.01
+epssilon_decay_value = 0.995
+epsilon = epssilon_starting_value
+scores_on_100_episodes = deque(maxlen=100)
+
+for episode in range(1, number_episozed+1):
+    state, _ = env.reset()# co to xdd
+    score = 0 
+    for t in range(0,maximum_number_timesteps_per_episodes):
+        action = agent.act(state, epsilon)
+        next_state, reward, done, _ , _ = env.step(action)
+        agent.step(state,action,reward,next_state,done)
+        state = next_state
+        score += reward
+        if done: 
+            break
+    scores_on_100_episodes.append(score)
+    epsilon = max(epssilon_ending_value, epssilon_decay_value*epsilon)
+    print('\rEpisode {}\tAverage Score: {:.2f}'.format(episode,np.mean(scores_on_100_episodes)), end="")
+    if episode % 100 == 0:
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(episode,np.mean(scores_on_100_episodes)))
+    if np.mean(scores_on_100_episodes) >= 200.0:
+        print('\Envierment solved in  {:d} episoed!\tAverage Score: {:.2f}'.format(episode-100,np.mean(scores_on_100_episodes)))
+        torch.save(agent.local_qnetork.state_dict(), 'checkpoint.pth')
+        break
+
+
+# # Ścieżka do pliku z modelem
+# model_file_path = 'checkpoint.pth'
+
+# # Sprawdzenie, czy plik z modelem istnieje
+# if os.path.isfile(model_file_path):
+#     # Wczytanie stanu modelu
+#     agent.local_qnetork.load_state_dict(torch.load(model_file_path))
+#     print("Model wczytany pomyślnie.")
+# else:
+#     print("Nie znaleziono pliku modelu, rozpoczynanie treningu od nowa.")
+        
+    
+
+
+# Part 3 - Visualizing the results
+
+import glob
+import io
+import base64
+import imageio
+from IPython.display import HTML, display
+from gym.wrappers.monitoring.video_recorder import VideoRecorder
+
+def show_video_of_model(agent, env_name):
+    env = gym.make(env_name, render_mode='rgb_array')
+    state, _ = env.reset()
+    done = False
+    frames = []
+    while not done:
+        frame = env.render()
+        frames.append(frame)
+        action = agent.act(state)
+        state, reward, done, _, _ = env.step(action.item())
+    env.close()
+    imageio.mimsave('video.mp4', frames, fps=30)
+
+show_video_of_model(agent, 'LunarLander-v2')
+
+def show_video():
+    mp4list = glob.glob('*.mp4')
+    if len(mp4list) > 0:
+        mp4 = mp4list[0]
+        video = io.open(mp4, 'r+b').read()
+        encoded = base64.b64encode(video)
+        display(HTML(data='''<video alt="test" autoplay
+                loop controls style="height: 400px;">
+                <source src="data:video/mp4;base64,{0}" type="video/mp4" />
+             </video>'''.format(encoded.decode('ascii'))))
+    else:
+        print("Could not find video")
+
+show_video()
